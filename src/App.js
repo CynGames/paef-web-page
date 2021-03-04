b55431ef660040b900a2af7c1ee3310a2b2e483a
CynGames <tomasm.leguizamon@gmail.com>
2021-03-04 03:14:22 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import './App.css';
-import SignIn from './Components/Sign In/SignIn';
-import SignUp from './Components/Sign Up/SignUp';
-import MainPage from './Components/Main Page/MainPage';
-import TableCreation from './Components/Table Creation/TableCreation';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import "./App.css";
+import SignIn from "./Components/Sign In/SignIn";
+import SignUp from "./Components/Sign Up/SignUp";
+import MainPage from "./Components/Main Page/MainPage";
+import TableCreation from "./Components/Table Creation/TableCreation";
+import StepCreation from "./Components/Table Creation/StepCreation";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App()
-{
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Router>
-      <div>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route exact path={"/signup"} component= {SignUp}/>
-          <Route exact path={"/mainpage"} component= {MainPage}/>
-          <Route exact path={"/tablecreation"} component= {TableCreation}/>
-          <Route exact path={"/"} component= {SignIn}/>
-        </Switch>
-      </div>
-      </Router>
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path={"/signup"} component={SignUp} />
+              <Route exact path={"/mainpage"} component={MainPage} />
+              <Route exact path={"/tablecreation"} component={TableCreation} />
+              <Route exact path={"/"} component={StepCreation} />
+              {/* <Route exact path={"/"} component={SignIn} /> */}
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
